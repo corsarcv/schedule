511b9776f81eac3527971de8c746b78e8b700a16
Ihor Derevianskyi <igor.cors@gmail.comCompiled>
2023-12-02 01:04:06 -0500

    fixed styles for weekends and holidays

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,68 +1,15 @@
 import { Component } from "react"; 
 import css from './Schedule.module.css'
+import { DateUtils } from "date-utils";
 
 export class Schedule extends Component{
     
-    getMonday = (d) => {
-        var today = d ? new Date(d): new Date();
-        var day = today.getDay() || 7; // Get current day number, converting Sun. to 7
-        if (day !== 1) 
-          today.setHours(-24 * (day - 1));
-        return today;
-      }
-    getNextMonday = (d) => {
-        var today = d ? new Date(d) : new Date();
-        var day = today.getDay() || 7; // Get current day number, converting Sun. to 7
-        today.setHours(24 * (8-day));
-        return today;
-    } 
+    dateUtils = new DateUtils(this.props.holidays);
 
-    getFormattedDate = (d) => {
-        return this.dayOfTheWeek(d) + ", " + d.getDate();
-    }
-
-    getSchoolDay = (d, currentUser=null) => {
-        if ([0,6].includes(d.getDay()))
-            return null;
-        if (!currentUser)
-            currentUser = this.findSelectedUser();
-        const daysDiff = this.dateDiffInDays(new Date(currentUser.day_one), d);
-        return daysDiff >= 0 ? daysDiff % 6 + 1 : 6 - Math.abs(daysDiff) % 6 + 1;
-    }
-
-    getFormattedSchoolDate = (d) => {
-        const schoolDayIndex = this.getSchoolDay(d);
-        return schoolDayIndex? "Day " + schoolDayIndex: null;
-    }
-    getPeriodForDate = (dt, index) => {
-        if (dt.getDay() == 6 || dt.getDay() == 0)
-            return null;
-        const currentUser = this.findSelectedUser();
-        const schoolDayIndex = this.getSchoolDay(dt, currentUser);
-        const keyName = 'day' + schoolDayIndex;        
-        const todaySchedule = currentUser.schedule[keyName];
-        return todaySchedule[index];
-    }
-
-    dayOfTheWeek = (d) => {
-        switch (d.getDay()){
-            case 0:
-                return 'Sun'
-            case 1:
-                return 'Mon'
-            case 2:
-                return 'Tue'
-            case 3:
-                return 'Wed'
-            case 4:
-                return 'Thu'
-            case 5:
-                return 'Fri'
-            case 6:
-                return 'Sat'
-            default:
-                return null;
-        }
+    state = {
+        startDate: this.dateUtils.getCurrentMonday(),
+        endDate: this.dateUtils.getNextMonday(),
+        periodsCount: 9
     }
      
     findSelectedUser(){
@@ -78,59 +25,17 @@ export class Schedule extends Component{
         return foundUser;
     }
 
-    getDatesList = () => {
-        var days = [];
-        var currentDate = new Date(this.state.startDate.getTime());
-        do {
-            const cloneDate =  new Date(currentDate.getTime() );
-            days.push(cloneDate);
-            currentDate.setDate(currentDate.getDate() + 1);
-        } while (currentDate <= this.state.endDate);
-        return days;
-    }
-
-    getFormattedPeriod = () =>{
-        const s = this.state.startDate;
-        const e = this.state.endDate
-        const startMonth = s.toLocaleString('default', { month: 'short' });
-        const endMonth = e.toLocaleString('default', { month: 'short' });
-        return `${s.getDate()} ${startMonth} - ${e.getDate()} ${endMonth}`
-    }
-    
-    dateDiffInDays = (a, b) => {
-        a.setHours(0,0,0,0); 
-        b.setHours(0,0,0,0); 
-        
-        if (a.toString() == b.toString()){
-            return 0;
-        }
-        const isPast = a>b;
-
-        const increment = isPast ? -1 : 1;
-        let count = 0;
-        let currentDate = new Date(a);
-        do {
-            currentDate.setDate(currentDate.getDate() + increment);
-            if (![6, 0].includes(currentDate.getDay()))
-                count += increment;
-        } while ((isPast && currentDate > b) || (!isPast && currentDate < b) || (count > 10))
-        return count;
-      }
-
-    state = {
-        startDate: this.getMonday(),
-        endDate: this.getNextMonday(),
-        periodsCount: 9 // Can be dynamic
-    }
-
     render () {
         const selectedUser = this.findSelectedUser();
+
         return <div className={css.calendar}>
         <header>
             <div className={[css.secondary, css.secondary_header].join(" ")} >Class {selectedUser.class}</div>
             <div className={css.calendar_title}>
               <div className={[css.icon, css.secondary, css.chevron_left].join(" ")}>‹</div>
-              <h1><strong>{this.getFormattedPeriod()}</strong></h1>
+              <h1><strong>
+                {this.dateUtils.getFormattedPeriod(this.state.startDate, this.state.endDate)}
+              </strong></h1>
               <div className={[css.icon, css.secondary, css.chevron_left].join(" ")}>›</div>
             </div> 
             <div className={css.secondary_header}></div>
@@ -143,21 +48,21 @@ export class Schedule extends Component{
         <thead>
           <tr>
             <th className={css.headcol}></th>
-            {this.getDatesList().map(dt =>(
-                <th key={this.getFormattedDate(dt)} className={
-                    [this.getFormattedDate(dt) == this.getFormattedDate(new Date())? css.today : "",
-                    (dt.getDay() == 0 || dt.getDay() == 6) ? css.secondary : ""].join(" ")}>
-                    {this.getFormattedDate(dt)}
+            {this.dateUtils.getDatesList(this.state.startDate, this.state.endDate).map(dt =>(
+                <th key={this.dateUtils.getFormattedDate(dt)} className={
+                    [this.dateUtils.getFormattedDate(dt) == this.dateUtils.getFormattedDate(new Date())? css.today : "",
+                    (this.dateUtils.isWeekendOrHoliday(dt)) ? css.secondary : ""].join(" ")}>
+                    {this.dateUtils.getFormattedDate(dt)}
                 </th>
             ))}
           </tr>
           <tr>
             <th className={css.headcol}></th>
-            {this.getDatesList().map(dt =>(
-                <th key={this.getFormattedDate(dt) + "_day"} className={
-                    [this.getFormattedDate(dt) == this.getFormattedDate(new Date())? css.today : "",
-                    (dt.getDay() == 0 || dt.getDay() == 6) ? css.secondary : ""].join(" ")}>
-                    {this.getFormattedSchoolDate(dt)}
+            {this.dateUtils.getDatesList(this.state.startDate, this.state.endDate).map(dt =>(
+                <th key={this.dateUtils.getFormattedDate(dt) + "_day"} className={
+                    [this.dateUtils.getFormattedDate(dt) == this.dateUtils.getFormattedDate(new Date())? css.today : "",
+                    (this.dateUtils.isWeekendOrHoliday(dt)) ? css.secondary : ""].join(" ")}>
+                    {this.dateUtils.getFormattedSchoolDate(dt, selectedUser.dayOne)}
                 </th>
             ))}
           </tr>
@@ -171,13 +76,13 @@ export class Schedule extends Component{
             return (
                 <tr key={`row_${i}`}>
                     <td className={css.headcol}>{i>0 ? i: "HR" }</td>
-                    {this.getDatesList().map(dt =>(
+                    {this.dateUtils.getDatesList(this.state.startDate, this.state.endDate).map(dt =>(
                         <td key={`${i}${dt.toString()}`} className={[
-                            [0,6].includes(dt.getDay())? css.past: null,
-                            css.event, css.chevron_left
+                                this.dateUtils.isWeekendOrHoliday(dt) ? css.past: null,
+                                css.event, css.chevron_left
                             ].join(' ')}>
                                 <ScheduleCellComponent 
-                                    period={this.getPeriodForDate(dt, i)} />   
+                                    period={this.dateUtils.getPeriodForDate(dt, i, selectedUser)} />   
                         </td>
                     ))}
               </tr>)
---
+++ src/date-utils.js
@@ -0,0 +1,87 @@
+export class DateUtils {
+    constructor(holidays) {
+        this.holidays = holidays;
+      }
+
+    getCurrentMonday (d) {
+        var today = d ? new Date(d): new Date();
+        var day = today.getDay() || 7; // Get current day number, converting Sun. to 7
+        if (day !== 1) 
+          today.setHours(-24 * (day - 1));
+        return today;
+      }
+    getNextMonday = (d) => {
+        var today = d ? new Date(d) : new Date();
+        var day = today.getDay() || 7; // Get current day number, converting Sun. to 7
+        today.setHours(24 * (8 - day));
+        return today;
+    } 
+
+    getFormattedDate = (d) => {
+        return d.toLocaleString('default', { weekday: 'short' }) + ", " + d.getDate();
+    }
+
+    getSchoolDay = (d, dayOne) => {
+        if ([0,6].includes(d.getDay()) || this.isHoliday(d))
+            return null;
+        const daysDiff = this.dateDiffInDays(new Date(dayOne), d);
+        return daysDiff >= 0 ? daysDiff % 6 + 1 : 6 - Math.abs(daysDiff) % 6 + 1;
+    }
+
+    getFormattedSchoolDate = (d, dayOne) => {
+        const schoolDayIndex = this.getSchoolDay(d, dayOne);
+        return schoolDayIndex? "Day " + schoolDayIndex: null;
+    }
+    getPeriodForDate = (dt, index, currentUser) => {
+        if (dt.getDay() == 6 || dt.getDay() == 0 || this.isHoliday(dt))
+            return null;
+        const schoolDayIndex = this.getSchoolDay(dt, currentUser.dayOne);
+        const keyName = 'day' + schoolDayIndex;        
+        const todaySchedule = currentUser.schedule[keyName];
+        return todaySchedule[index];
+    }
+
+    getDatesList = (startDate, endDate) => {
+        var days = [];
+        var currentDate = new Date(startDate.getTime());
+        do {
+            const cloneDate =  new Date(currentDate.getTime() );
+            days.push(cloneDate);
+            currentDate.setDate(currentDate.getDate() + 1);
+        } while (currentDate <= endDate);
+        return days;
+    }
+
+    getFormattedPeriod = (startDate, endDate) =>{
+        const startMonth = startDate.toLocaleString('default', { month: 'short' });
+        const endMonth = endDate.toLocaleString('default', { month: 'short' });
+        return `${startDate.getDate()} ${startMonth} - ${endDate.getDate()} ${endMonth}`
+    }
+    
+    isHoliday = (d) => {
+        return this.holidays.includes(d.toLocaleDateString());
+    }
+
+    isWeekend = (d) => {
+        return [0, 6].includes(d.getDay())
+    }
+
+    isWeekendOrHoliday = (d) => {
+        return this.isWeekend(d) || this.isHoliday(d);
+    }
+
+    dateDiffInDays = (a, b) => {
+        a.setHours(0,0,0,0); 
+        b.setHours(0,0,0,0); 
+        const isPast = a > b;
+        const increment = isPast ? -1 : 1;
+        let count = 0;
+        let currentDate = new Date(a);
+        while ((isPast && currentDate > b) || (!isPast && currentDate < b) || (count > 10)) {
+            currentDate.setDate(currentDate.getDate() + increment);
+            if (!this.isWeekendOrHoliday(currentDate))
+                count += increment;
+        } 
+        return count;
+      } 
+}
