dba44e91922ff52c41569bf86478b7172aa53e88
Ihor Derevianskyi <igor.cors@gmail.comCompiled>
2023-12-02 01:13:39 -0500

    fixed warnings

--- src/components/Chips.jsx
+++ src/components/Chips.jsx
@@ -10,9 +10,9 @@ export class Chips extends Component{
     render () {
         return <div className={css.chips} >
             {this.props.users.map(user=>(
-                <div className={[css.chip, this.props.selectedUser == user.id ? css.selected: ''].join(' ')} 
+                <div className={[css.chip, this.props.selectedUser === user.id ? css.selected: ''].join(' ')} 
                 key={user.id} data-key={user.id} onClick={this.props.clickHandler}>
-                    <img src={user.sex == 'M'? avatarM: avatarF} alt="Person" width="96" height="96" />
+                    <img src={user.sex === 'M'? avatarM: avatarF} alt="Person" width="96" height="96" />
                     {user.name}
                 </div>
                 ))
